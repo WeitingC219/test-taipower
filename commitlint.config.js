2de4a77705032ae5ab35603ecd4157552895e932
Wei-Ting Chien <weitingc219@gmail.com>
2025-12-01 16:34:58 +1100

    chore(tooling): setup eslint, prettier, husky, commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,34 @@
 module.exports = {
+  // 用官方的 conventional 規則
   extends: ['@commitlint/config-conventional'],
+
+  // 想要更嚴可以在 rules 自訂
+  rules: {
+    // 限制 type 一定要用這幾種（可自行調整）
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
+    // summary 不可為空
+    'subject-empty': [2, 'never'],
+    // 不讓 subject 結尾有句點
+    'subject-full-stop': [2, 'never', '.'],
+    // 強制小寫 type
+    'type-case': [2, 'always', 'lower-case'],
+    // scope 可以選填，但如果寫要是小寫
+    'scope-case': [2, 'always', 'lower-case'],
+  },
 };
