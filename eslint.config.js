e9f0379a9098289b9604f5c7ce1bf20fe2728f33
WeitingC219 <52922454+WeitingC219@users.noreply.github.com>
2025-12-01 18:33:38 +1100

    chore(tooling): setup project tooling (eslint, prettier, husky, commitlint, lint-staged, tsconfig) (#1)
    * chore(tooling): setup eslint, prettier, husky, commitlint
    * chore(tooling): setup eslint, prettier, husky, commitlint

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const importPlugin = require('eslint-plugin-import-x');
+const eslintConfigPrettier = require('eslint-config-prettier');
+
+module.exports = tseslint.config(
+  {
+    ignores: [
+      'dist/**',
+      'node_modules/**',
+      'eslint.config.js',
+      'commitlint.config.js',
+    ],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+    settings: {
+      'import-x/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.json',
+        },
+      },
+    },
+    rules: {
+      'no-console': 'warn',
+    },
+  },
+);
