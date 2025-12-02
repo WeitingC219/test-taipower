263963260b114e570901ca5512226f33c1ba6d19
Wei-Ting Chien <weitingc219@gmail.com>
2025-12-02 12:46:53 +1100

    test(tooling): put back

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
 import http from 'http';
 
 const server = http.createServer((req, res) => {
-  // Vulnerability 1: Gitleaks (Fake AWS Key)
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const awsKey = 'AKIAIOSFODNN7EXAMPLE';
-
-  // Vulnerability 2: CodeQL (Remote Code Execution via eval)
-  const userInput = req.url?.split('?')[1] ?? '';
-
-  eval(userInput);
-
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('hello world7');
@@ -19,12 +10,8 @@ const server = http.createServer((req, res) => {
   }
 });
 
-// Only start the server if this file is run directly
-if (require.main === module) {
-  server.listen(3000, () => {
-    // eslint-disable-next-line no-console
-    console.log('Server running at http://localhost:3000/');
-  });
-}
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
 
 export default server;
